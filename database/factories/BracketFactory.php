b33aaaa3429854697ff61d62605e26206e463c13
sebby-omarchy <sebgorgone@gmail.com>
2026-09-17 10:28:16 -0400

    init

---
+++ database/factories/BracketFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Bracket;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Bracket>
+ */
+class BracketFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
